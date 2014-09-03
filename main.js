c3794fd88246cbf9ecbcd05014b28029579282d1
Allan Monzon <allanjmonzon@gmail.com>
2014-09-03 14:54:54 -0400

    JS

---
+++ main.js
@@ -0,0 +1,37 @@
+function myFunction() {
+	document.getElementById('update-button').onclick = function(){
+		var thimbles = document.getElementById('thimbles').value;
+		var swords = document.getElementById('swords').value;
+		var x = Number(thimbles);
+		var y = Number(swords);
+		var sum = x + y * 37;
+		sum.toString();
+		var total = sum + ' monies';
+  		document.getElementById('the-total').textContent = total;
+	};
+};
+
+myFunction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
